3d1624d5e2e3aa92ee6c23c1ff6cb1023a375fb9
Szymon <szymon.rysztof@eengine.pl>
2023-04-24 22:58:43 +0200

    Switched prints to proper logging information

--- app/influx_writer.py
+++ app/influx_writer.py
@@ -1,23 +1,21 @@
-#!/usr/bin/env python3 
-import logging, json
+#!/usr/bin/env python3
+import logging
+import json
+from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client import InfluxDBClient, Point
 from config import Config
 logger = logging.getLogger(__name__)
 
 
-from influxdb_client import InfluxDBClient, Point, WriteOptions
-from influxdb_client.client.write_api import SYNCHRONOUS
-
 
 def main(data):
     data = json.loads(json.dumps(data, indent=4))
-    influx = InfluxDBClient(url=Config.influx_url, token=Config.influx_token, org=Config.influx_org)
-    write_api = influx.write_api(write_options=SYNCHRONOUS)
-    
+
     writer('disks', data)
     writer('cpu', data)
     writer('ram', data)
     writer('swap', data)
-   
+
 
 def writer(metric, data):
     if metric == 'disks':
@@ -32,16 +30,16 @@ def writer(metric, data):
                     try:
                         writer.write(bucket=Config.influx_bucket, record=[point])
                     except InfluxDBError as e:
-                        print(e)
+                        logging.exception(f"Error while writing to influxdb: {e}")
     else:
         point = Point(metric)
         point.tag("Metric", metric)
         point.tag("Hostname", Config.hostname)
         for key, value in data[metric].items():
-            point.field(key, value) 
+            point.field(key, value)
         with InfluxDBClient(url=Config.influx_url, token=Config.influx_token, org=Config.influx_org) as client:
             with client.write_api(write_options=SYNCHRONOUS) as writer:
                 try:
                     writer.write(bucket=Config.influx_bucket, record=[point])
                 except InfluxDBError as e:
-                    print(e)       
+                    logging.exception(f"Error while writing to influxdb: {e}")
